297bce4149d744bf27c7190126cb68ba562b7fa8
Daniel <daslomo@gmail.com>
2025-02-12 12:06:50 +0100

    Corrected "passsord" in variables

--- Generic-Test-Cases/variables.py
+++ Generic-Test-Cases/variables.py
@@ -13,7 +13,7 @@ password = "test12345"
 username_field = "id:reg-username"
 password_field = "id:reg-password"
 login_username_field = "id:login-username"
-login_password_field = "id:login-password"
+login_password_field = "id:login-passsord"
 
 submit_register = "css:#register-form button[type='submit']"
 submit_login = "css:#login-form > button[type='submit']"
@@ -30,4 +30,3 @@ book_safaris_button = "id:safari-nav"
 cart_button = "id:cart-nav"
 
 # endregion
-
