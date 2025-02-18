da1680ae27f3c390d7fb00abf83d18becf1a344c
Tan-Thai <134406558+Tan-Thai@users.noreply.github.com>
2025-02-18 16:45:12 +0100

    Send keys variant for selecting book tour date (fix) (#33)
    * feat: Added functionality for keyword pressing when selecting a calendar
    
    - 1 test added in generic.robot starting at line 57
    - The test is a duplicate of "They add a viable tour with a chosen date to the cart", with the exeption of navigating the calendar instead of typing the date into the text box
    
    - 3 functions added in generic-keywords.robot, with a working name starting from line 132
    - They are placed subsequently in order temporarily, until fully functional
    
    - 2 functions added in variables.py, subsequently starting at line 11
    - They are called in a chain to fetch the amount of days left until that day next week, taking the name of a weekday as an argument
    
    - Currently not working, due to generic-keywords not being able to call function "get_days_left_until_day"
    - Provided commentary in generic-keywords at the point the new test fails
    
    * fix(Date Selection): Fixed date picker in dynamic calendar ui from JS (#32)
    
    * fix(Date Selection): Fixed date picker in dynamic calendar ui from javascript
    
    - added weekday_helper.py to resources for all things calendar calc.
        - added funcs within these that replaces old ones from variables + generic-keywords
    - Commented away old code and moved to the bottom. safekeeping for the sake of history.
    
    * style: Added clarity to keywords, removed old comments, renamed variables
    
    ---------
    
    Co-authored-by: Daniel Kvist <daslomo@gmail.com>

--- Generic-Test-Cases/generic.robot
+++ Generic-Test-Cases/generic.robot
@@ -37,21 +37,29 @@ User logs out successfully
     Then They should be redirected to the homepage and see the login button
 
 # Adding a tour/ticket to the cart both tests the carts functions so a dedicated test might not be needed
+# Andreas made a test for cart specifically initially, but is now null due to the other cart tests.
 User adds a ticket to the cart
-    # TODO: Check if this test needs to be reworked.
-    # The previous iteration of this only added items to the cart, it never purchased it. Should we add that?
     [Tags]    Daniel    Tan_Refactor
     [Documentation]    Assures that the user is able to purchase a ticket when they are logged in.
     Given The User Is Logged In
-    When They add a 'REGULAR' ticket to the cart    # NORMAL/VIP for ticket type choices
+    #TODO: make the regular check more dynamic (similar to date check in tour.)
+    When They add a 'REGULAR' ticket to the cart    # REGULAR/VIP for ticket type choices
     Then They should be able to see the ticket in the cart
 
 User adds a tour to the cart
     [Tags]    Andreas    Tan_refactor
     [Documentation]    Assures that the user is able to purchase a tour when they are logged in.
     Given The User Is Logged In
+    And They Add A 'VIP' Ticket To The Cart
+    When They add a tour booked for next SuNday by navigating the calendar dropdown using the keyboard to the cart
+    Then They should be able to see the tour in the cart
+
+User adds a tour using keyboard to the cart
+    [Tags]    Andreas    Tan_refactor
+    [Documentation]    Assures that the user is able to purchase a tour when they are logged in.
+    Given The User Is Logged In
     And They Add A 'REGULAR' Ticket To The Cart
-    When They add a viable tour with a chosen date to the cart
+    When They add a tour booked for next tuesday by navigating the calendar dropdown using the keyboard to the cart
     Then They should be able to see the tour in the cart
 
 User completes a purchase
--- Generic-Test-Cases/variables.py
+++ Generic-Test-Cases/variables.py
@@ -7,35 +7,13 @@ browser = "chrome"
 url = f"file:///{current_directory}/resources/website/jurap.html"
 # endregion
 
-# region Functions
-def get_next_upcoming_weekday(target_weekday, date_format="YYYY-MM-DD"):
-    #https://www.geeksforgeeks.org/python-program-to-get-the-nth-weekday-after-a-given-date/
-    #0-6 for the days of the week, 0=monday -> 6=sunday
-    current_date = datetime.date.today()
-    days_delta = target_weekday - current_date.weekday()
-    if days_delta <= 0:
-        days_delta += 7
-
-    date_of_next_weekday = current_date + datetime.timedelta(days_delta)
-
-    # reasoning behind MM-DD-YY is due to the websites interpretation of input. it being en-us
-    # reformating the date from YY-MM-DD --> to MM-DD-YY
-    # https://docs.python.org/3.13/library/datetime.html#format-codes
-    if date_format == "MM-DD-YYYY":
-        return date_of_next_weekday.strftime("%m-%d-%Y")
-    elif date_format == "YYYY-MM-DD":
-        return date_of_next_weekday.strftime("%Y-%m-%d")
-    else:
-        raise ValueError("Invalid date_format. Use 'DD-MM-YYYY' or 'YYYY-MM-DD'.")
+# region Home Variables
+home_section = '//*[@id="home-section"]'
 # endregion
 
-# these sections should probably be renamed to 'page'
-# random shit by Tan - will move when all tests are done. refactor my refactor yeyeyeyeye.
-home_section = '//*[@id="home-section"]'
-
 # region Credentials variables
-username = "test123"
-password = "test12345"
+valid_username = "test123"
+valid_password = "test12345"
 
 invalid_username = "123test"
 invalid_password = "12345test"
@@ -75,10 +53,9 @@ add_ticket_to_cart_button = "css:#ticket-form > button[type='submit']"
 # region Safari variables
 safari_section = '//*[@id="safari-section"]'
 
-ticket_type_dropdown = "id:ticket-category"
+ticket_category_dropdown = "id:ticket-category"
 safari_type_dropdown = "id:safari-type"
 safari_date_input_field = "xpath://input[@id='safari-date']"
-next_monday_date = get_next_upcoming_weekday(0,"MM-DD-YYYY" ) # argument is 0-6
 
 add_safari_to_cart_button = "css:#safari-form > button[type='submit']"
 # endregion
@@ -90,7 +67,6 @@ cart_details = "id:cart-details"
 item_added_to_cart_message_text = "Item added to cart!"
 
 next_monday_date_TEST = "02-28-2025" ## I DON'T GET THIS ONE, WHY IS AMERICAN INPUT FUNCTIONING HERE??? -TT
-expected_monday_date_in_cart = get_next_upcoming_weekday(0, "YYYY-MM-DD")
 # endregion
 
 # region Checkout variables
